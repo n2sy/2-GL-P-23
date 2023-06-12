777dcb4e0b346de63fc2f75bb534a361747bf8d5
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-12 16:38:12 +0100

    first commit

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent {
+  section = '3-GL-P';
+  university: string = 'Tek-up';
+  color = 'pink';
+  hd = false;
+
+  traitement() {
+    //alert('Click détecté');
+    this.university = 'Sésame';
+  }
+}
